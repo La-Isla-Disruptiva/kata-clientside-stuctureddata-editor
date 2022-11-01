117f69673cb45cfb8aee3625d6260be84baa2524
Hugo B. Cerda Minaya <hcerda@strappcorp.com>
2022-11-01 17:48:43 -0600

    Added functionality to validate markdown and yaml separately, keeping track of the title, annotation and content if both are valid

--- src/actions.js
+++ src/actions.js
@@ -3,6 +3,7 @@ import { useObservable } from "@vueuse/rxjs";
 import { db } from "./db";
 import Ajv from "ajv";
 import yaml from "js-yaml";
+import markdownlint from "markdownlint";
 
 const ajv = new Ajv();
 const schema = {
@@ -17,8 +18,10 @@ const schema = {
 }
 
 export default class apiModule{
-    addItemToDB(title){
-       db.documents.add({ title: title})
+    addItemToDB(title, annotation, content){
+       db.documents.add({ title: title,
+                          annotation: annotation,
+                          content: content})
     }    
 
     getItemsFromDB(){
@@ -36,21 +39,48 @@ export default class apiModule{
         db.documents.delete(item)
     }
 
-    validateYaml(title){
-       let jsobj = yaml.load(title.trim())
-       let validate = ajv.compile(schema)
-       let valid = validate(jsobj)
-       if (!valid) {
-         return false;
-       }else {
-         this.addItemToDB(title);
-         return true;
-       }
-        
+    validateYaml(annotation){
+      let jsobj = yaml.load(annotation.trim())
+      let validate = ajv.compile(schema)
+      let valid = validate(jsobj)
+      if (!valid) {
+        return false;
+      }else {
+        return true;
+      }
     }
 
-    validateMarkdown(){
-        console.log('inside validate markdown')
+    validateMarkdown(content){
+      const options = {
+        "strings": {
+          "content": content
+        }
+      };
+  
+      let band = false;
+      let errMessage = '';
+
+      markdownlint(options, function callback(err, result) {
+        if (!err) {
+          const resultString = (result || "").toString();
+          if (resultString) {
+            band = false;
+            errMessage = resultString
+          }else{
+            band = true;
+          }
+        }
+      });
+
+      if(content == ''){
+        return false;
+      }
+
+      if(band){
+        return true;
+      }else{
+        return false;
+      }
     }
 
     
